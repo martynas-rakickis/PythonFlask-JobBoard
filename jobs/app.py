489a0ba2c46fe181f2716c52d5203b7737d9611e
martynas-rakickis <martynas.rakickis@telia.lt>
2023-01-25 09:32:20 +0200

    module 6

--- jobs/app.py
+++ jobs/app.py
@@ -31,7 +31,7 @@ def jobs():
     return render_template('index.html', jobs=jobs, reviews=reviews)
 
 @app.route('/employer/<employer_id>')
-def employer():
+def employer(employer_id):
     employer = execute_sql('SELECT * FROM employer WHERE id=?',[employer_id], single=True)
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id])
     return render_template('employer.html', employer=employer, jobs = jobs)
